4ca4456c2baa3a23886a59d02929d103e8435501
eong98 <dndnjs6918@gmail.com>
2026-08-03 15:49:53 +0900

    열심히합시다.

---
+++ src/test/java/dev/jpa/allimio/Team2JpaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jpa.allimio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2JpaV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
